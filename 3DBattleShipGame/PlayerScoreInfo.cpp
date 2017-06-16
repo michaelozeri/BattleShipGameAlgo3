8a0bf2eaf79a36e366a5595aa758b96db9bafdc1
mzatielr <mordehaizai@gmail.com>
2017-06-17 01:21:35 +0300

    Allgned print implementation

--- 3DBattleShipGame/PlayerScoreInfo.cpp
+++ 3DBattleShipGame/PlayerScoreInfo.cpp
@@ -1,4 +1,9 @@
 #include "PlayerScoreInfo.h"
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
 
 PlayerScoreInfo::PlayerScoreInfo(int playerId, std::string name): PlayerId(playerId), GamePlayed(0), Wins(0), Losses(0), PtsFor(0), PtsAgainst(0)
 {
@@ -17,6 +22,43 @@ void PlayerScoreUtils::UpdatePlayerScores(vector<PlayerScoreInfo>& scores, vecto
 		currentPlayer.PtsFor += elem.PtsFor;
 		currentPlayer.PtsAgainst += elem.PtsAgainst;
 	}
+
+	PrintScores(cout, scores);
 }
 
+void PlayerScoreUtils::PrintScores(ostream& out, const vector<PlayerScoreInfo>& scores)
+{
+	PrintStartLine(out);
+	int list_index = 1;
+	for each(const PlayerScoreInfo& score in scores)
+	{
+		PrintSingleScore(out, score, list_index);
+		++list_index;
+	}
+}
 
+void PlayerScoreUtils::PrintSingleScore(ostream& out, const PlayerScoreInfo& score, int list_index)
+{
+	stringstream ss;
+	ss << list_index << ".";
+	out << left << setw(indexW) << ss.str();
+	out << left << setw(NameW) << score.Name;
+	out << left << setw(WinsW) << score.Wins;
+	out << left << setw(LossesW) << score.Losses;
+	out << left << setw(PercentageW) << 50;
+	out << left << setw(PtsForW) << score.PtsFor;
+	out << left << setw(PtsAgainst) << score.PtsAgainst;
+	out << endl;
+}
+
+void PlayerScoreUtils::PrintStartLine(ostream& out)
+{
+	out << left << setw(indexW) << "#";
+	out << left << setw(NameW) << "Team Name";
+	out << left << setw(WinsW) << "Wins";
+	out << left << setw(LossesW) << "Losses";
+	out << left << setw(PercentageW) << "%";
+	out << left << setw(PtsForW) << "Pts For";
+	out << left << setw(PtsAgainst) << "Pts Against";
+	out << endl << endl;
+}
--- 3DBattleShipGame/PlayerScoreInfo.h
+++ 3DBattleShipGame/PlayerScoreInfo.h
@@ -21,6 +21,19 @@ public:
 
 class PlayerScoreUtils
 {
+	//weight definitions
+	static const int indexW = 8;
+	static const int NameW = 24;
+	static const int WinsW = 8;
+	static const int LossesW = 8;
+	static const int PercentageW = 8;
+	static const int PtsForW = 10;
+	static const int PtsAgainst = 11;
+
+
 public:
 	static void UpdatePlayerScores(vector<PlayerScoreInfo>& scores, vector<PlayerResultElement> elementsToUpdate);
+	static void PrintScores(ostream& out, const vector<PlayerScoreInfo>& scores);
+	static void PrintSingleScore(ostream& out, const PlayerScoreInfo& score, int list_index);
+	static void PrintStartLine(ostream& out);
 };
