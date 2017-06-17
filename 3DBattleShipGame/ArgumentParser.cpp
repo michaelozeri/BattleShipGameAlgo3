c1d2f45959e08f8d756bc4204247c27e1637f553
mzatielr <mordehaizai@gmail.com>
2017-06-17 21:43:03 +0300

    implement argument

--- 3DBattleShipGame/ArgumentParser.cpp
+++ 3DBattleShipGame/ArgumentParser.cpp
@@ -1,25 +1,29 @@
 #include "IArgumentParser.h"
+#include "IFileDirectoryUtils.h"
 
 #define DefaultThreadNum 4
 
 void IArgumentParser::ParseArguments(Configuration& config, int argc, char* argv[])
 {
+	config.thread_num = DefaultThreadNum;
+	config.path = "";
 	if (argc > 1)
 	{
-		config.thread_num = DefaultThreadNum;
-		for (int i = 1; i < argc; i++)
+		for (int i = 1; i < argc; ++i)
 		{
 			string s = argv[i];
 			if (!s.compare("-threads"))
 			{
 				int num = atoi(argv[i + 1]);
 				config.thread_num = num > 0 ? num : config.thread_num;
-				i++;
+				++i;
 			}
 			else
 			{
 				config.path = argv[i];
+				MainLogger << "IArgumentParser Path: " << config.path << endl;
 			}
 		}
 	}
+	MainLogger << "IArgumentParser: Threads- " << config.thread_num << endl;
 }
--- 3DBattleShipGame/main.cpp
+++ 3DBattleShipGame/main.cpp
@@ -25,8 +25,6 @@ int main(int argc, char* argv[])
 
 	Configuration config;
 	IArgumentParser::ParseArguments(config, argc, argv);
-	config.path = "C:\\Temp\\Foo1";
-	config.thread_num = 4;
 	GameContoller gameController(config);
 
 	gameController.RunApplication();
